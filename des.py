2acd5e16307583d2267bd3bd1cc6c67a712d950a
Mr.Cloudy <mr.cloudy.nsk@gmail.com>
2025-07-23 12:02:23 +0700

    Initial commit

---
+++ des.py
@@ -0,0 +1,16 @@
+import numpy as np
+number = np.random.randint(1, 101)# загадываем число
+print(number)
+count = 0
+while True:
+    count += 1
+    predict_number = int(input("Введите число от 1 до 100:"))
+    if predict_number > number:
+        print("Ваше число больше!")
+    elif predict_number < number:
+        print("Ваше число меньше!")
+    else:
+        print(f"Верно! Попыток затрачено {count}")
+        break
+
+            
